b9acaa85bcdfc56f78ff2a2eda48984036325470
Liviu Mitrofan <64259428+Liviujmk@users.noreply.github.com>
2022-10-19 21:22:00 +0300

    Add files via upload

---
+++ PCLP/main.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    // PROBLEME LAB 2 - Tema 21.10
+    //1
+    /*printf("Mitrofan Liviu \nGrupa 3111A \nCalculatoare");*/
+
+    //2
+    /*printf("NrMatricol \tStudent \tMedia\n");
+    printf("111 \t\tPopescu Ion \t10\n");
+    printf("2 \t\tIonescu Ana \t9.75\n");
+    printf("35 \t\tPopa Dan \t8.45\n");*/
+
+    //3
+    //printf("%o-%x", 123, 123);
+
+    //4
+    /*int x;
+    scanf("%d", &x);
+    printf("%d", x);*/
+
+    //5
+    /*int x,y;
+    scanf("%d%d", &x,&y);
+    printf("x:%d\ny:%d", x,y);*/
+
+    //6
+    /*int x,y;
+    scanf("%d%d", &x,&y);
+    printf("Suma:%d\nMedia:%d", x+y,(x+y)/2);*/
+
+    //7
+    /*int x;
+    scanf("%d", &x);
+    printf("Perimetrul patratului:%d\nAria patratului:%d", x*4,x*x);*/
+
+    //8
+    /*float L,l;
+    scanf("%f%f", &L,&l);
+    printf("Perimetrul dreptunghiului:%.2f\nAria dreptunghiului:%.2f", (2*L+2*l),L*l);*/
+
+    //9
+    /*int x,y,z;
+    scanf("%d%d%d", &x,&y,&z);
+    printf("%d", (x*y)/2);*/
+
+    //10
+    /*int x;
+    scanf("%d", &x);
+    printf("%d", abs(x-4)+abs(5-x));*/
+
+    //11
+    /*int x;
+    scanf("%d", &x);
+    float f1 = 8*x-16;
+    float f2 = x*x+x+1;
+    float f3 = x*x-1;
+    float f4 = x*x+3*x+5;
+    printf("Afisare cu notatie stiintifica: %E", (f1/f2)*(f3/f4));*/
+
+    //12
+    /*float raza, pi=3.141;
+    scanf("%f", &raza);
+    printf("Lungimea cercului: %.3f\nAria cercului: %.3f", 2*pi*raza, pi*(raza*raza));*/
+
+    //13
+    /*char un_caracter='a';
+    int un_numar=2, vector[10] = {5};
+    float un_numar_real = 3.14;
+    double un_alt_numar_real = 5.654;
+    long int x;
+    long long y;
+    printf("%d\n%d\n%d\n%d\n%d\n%d\n%d", sizeof(un_caracter), sizeof(un_numar), sizeof(vector),sizeof(un_numar_real), sizeof(un_alt_numar_real), sizeof(x), sizeof(y));*/
+
+    //14
+    /*float n1, n2, n3;
+    time_t t = time(NULL);
+
+    scanf("%f%f%f", &n1,&n2,&n3);
+    printf("\nCARNET NOTE\n");
+    printf("Data si ora curenta: %s", ctime(&t));
+    printf("\Student: Mitrofan Liviu\n\n");
+    printf("Nr. Crt \tDisciplina\tNota examen\n");
+    printf("---------------------------------------\n");
+    printf("1.\t\tPCLP\t\t%.2f\n", n1);
+    printf("2.\t\tGAC\t\t%.2f\n", n2);
+    printf("3.\t\tAM\t\t%.2f\n\n", n3);
+    printf("Media pentru semestrul I este: %.2f\n\n", (n1+n2+n3)/3);
+    return 0;*/
+}
